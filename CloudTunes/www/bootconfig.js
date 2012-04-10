a5dbbe35ab4d90ebae37948f3d3875fb600f237d
Tom Gersic <tgersic@modelmetrics.com>
2012-04-10 13:53:51 -0500

    setup for non-offline usage

--- CloudTunes/www/bootconfig.js
+++ CloudTunes/www/bootconfig.js
@@ -10,7 +10,7 @@ var debugMode = true;
 
 // The client ID value specified for your remote access object that defines
 // your application in Salesforce.
-var remoteAccessConsumerKey = "3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa";
+var remoteAccessConsumerKey = "3MVG9rFJvQRVOvk5eFQEwN9RO2aYtZg1lCMZuesA7mLAno.g_Kj4z_As_oAPc4gOYhpQkHoJ8AGnIOEHBwEzj";
 
 // The redirect URI value specified for your remote access object that defines
 // your application in Salesforce.
@@ -23,8 +23,8 @@ var oauthScopes = ["visualforce","api"];
 // PhoneGap-based application, and SFHybridApp.RemoteAppStartData for a Visualforce-based
 // application.  The default representations are below, or you can look at the data
 // classes in SFHybridApp.js to see how you can further customize your options.
-var startData = new SFHybridApp.LocalAppStartData();  // Used for local REST-based "index.html" PhoneGap apps.
-//var startData = new SFHybridApp.RemoteAppStartData("/apex/BasicVFPage"); // Used for Visualforce-based apps.
+//var startData = new SFHybridApp.LocalAppStartData();  // Used for local REST-based "index.html" PhoneGap apps.
+var startData = new SFHybridApp.RemoteAppStartData("/apex/cloudtunes?context=container"); // Used for Visualforce-based apps.
 
 // Whether the container app should automatically refresh our oauth session on app foreground:
 // generally a good idea.
